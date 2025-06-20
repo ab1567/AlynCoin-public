50dd7de2dd4c9fff9ccca602dc472c07225ed20e
ab1567 <60552839+ab1567@users.noreply.github.com>
2025-06-20 20:27:29 +0530

    Update constants.h

--- src/constants.h
+++ src/constants.h
@@ -7,3 +7,6 @@ inline constexpr std::string_view GENESIS_PARENT_HASH =
     "0000000000000000000000000000000000000000000000000000000000000000";
 
 inline constexpr int DESYNC_THRESHOLD = 5000;
+
+inline constexpr std::size_t MAX_SNAPSHOT_CHUNK_SIZE = 32 * 1024; // 32 KiB
+inline constexpr std::size_t MAX_PEERS               = 32;        // hard cap
