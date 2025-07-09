c04675512f72d6e5f186ad7203f1ee7cd8d6f8f9
ab1567 <60552839+ab1567@users.noreply.github.com>
2025-07-09 23:25:56 +0530

    Make BlockMeta public for helper functions

--- src/unit/reorg.test.cpp
+++ src/unit/reorg.test.cpp
@@ -4,48 +4,16 @@ int main() {
     Blockchain &bc = Blockchain::getInstanceNoDB();
     bc.clearChain();
 
-    Block g;
-    g.setIndex(0);
-    g.setPreviousHash(std::string(GENESIS_PARENT_HASH));
-    g.setHash(std::string(64, 'a'));
-    g.setDifficulty(1);
-    assert(bc.acceptBlock(g));
+    std::string prev = std::string(GENESIS_PARENT_HASH);
+    for(int i=0;i<10;i++) {
+        Block blk; blk.setIndex(i); blk.setPreviousHash(prev); blk.setHash(std::string(64,'a'+i)); blk.setDifficulty(1); assert(bc.acceptBlock(blk)); prev = blk.getHash();
+    }
+    std::string forkPrev = std::string(64,'a'+8); // block at height 8
+    Block x1; x1.setIndex(9); x1.setPreviousHash(forkPrev); x1.setHash(std::string(64,'x')); x1.setDifficulty(2); assert(bc.acceptBlock(x1));
+    Block x2; x2.setIndex(10); x2.setPreviousHash(std::string(64,'x')); x2.setHash(std::string(64,'y')); x2.setDifficulty(2); assert(bc.acceptBlock(x2));
+    Block x3; x3.setIndex(11); x3.setPreviousHash(std::string(64,'y')); x3.setHash(std::string(64,'z')); x3.setDifficulty(2); assert(bc.acceptBlock(x3));
 
-    Block b;
-    b.setIndex(1);
-    b.setPreviousHash(std::string(64, 'a'));
-    b.setHash(std::string(64, 'b'));
-    b.setDifficulty(1);
-    assert(bc.acceptBlock(b));
-
-    Block c;
-    c.setIndex(2);
-    c.setPreviousHash(std::string(64, 'b'));
-    c.setHash(std::string(64, 'c'));
-    c.setDifficulty(1);
-    assert(bc.acceptBlock(c));
-
-    Block bp;
-    bp.setIndex(1);
-    bp.setPreviousHash(std::string(64, 'a'));
-    bp.setHash(std::string(64, 'd'));
-    bp.setDifficulty(2);
-    assert(bc.acceptBlock(bp));
-
-    Block cp;
-    cp.setIndex(2);
-    cp.setPreviousHash(std::string(64, 'd'));
-    cp.setHash(std::string(64, 'e'));
-    cp.setDifficulty(2);
-    assert(bc.acceptBlock(cp));
-
-    Block dp;
-    dp.setIndex(3);
-    dp.setPreviousHash(std::string(64, 'e'));
-    dp.setHash(std::string(64, 'f'));
-    dp.setDifficulty(2);
-    assert(bc.acceptBlock(dp));
-
-    assert(bc.getLatestBlock().getHash() == std::string(64, 'f'));
+    assert(bc.getLatestBlock().getHash() == std::string(64,'z'));
+    assert(bc.getReorgCount() == 1);
     return 0;
 }
