1c1fc2fca1516fc8631575f42f89b3c43779e118
ab1567 <60552839+ab1567@users.noreply.github.com>
2025-08-03 10:03:53 +0530

    Use miniupnpc compat shim for legacy symbols

---
+++ src/upnp_compat.h
@@ -0,0 +1,9 @@
+#ifndef ALYNCOIN_UPNP_COMPAT_H
+#define ALYNCOIN_UPNP_COMPAT_H
+
+#define MINIUPNPC_SET_OLDNAMES
+#include <miniupnpc/miniupnpc.h>
+#include <miniupnpc/upnpcommands.h>
+#include <miniupnpc/upnperrors.h>
+
+#endif // ALYNCOIN_UPNP_COMPAT_H
