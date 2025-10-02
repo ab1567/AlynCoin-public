a3804239c7753e5e0079a02371d9542660cd0a7b
ab1567 <60552839+ab1567@users.noreply.github.com>
2025-10-02 20:11:11 +0530

    Revert "Add legacy snapshot fallback for older peers"

--- src/network.h
+++ src/network.h
@@ -287,9 +287,6 @@ private:
   void recordEndpointFailure(const std::string &host, int port);
   void recordSelfEndpoint(const std::string &host, int port);
   void refreshLocalInterfaceCache();
-  bool sendLegacySnapshot(const std::string &peerId,
-                          std::shared_ptr<Transport> transport,
-                          int upToHeight);
   bool shouldServeHeavyData(const std::string &peerId,
                             int remoteHeightHint = -1);
   void noteBlockRequested(const std::string &peer, const std::string &hash);
